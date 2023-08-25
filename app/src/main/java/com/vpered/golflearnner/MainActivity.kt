fe3d307f944dd50899475fcf74fd3e6bc86a4651
Yurii Svetliakov <mroallll54@gmail.com>
2023-08-25 17:56:12 +0300

    Добавил кнопку с диалогом

--- app/src/main/java/com/vpered/golflearnner/MainActivity.kt
+++ app/src/main/java/com/vpered/golflearnner/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.vpered.golflearnner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 import com.vpered.golflearnner.ui.theme.GolfLearnerTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +44,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        Greeting("Android")
+                        FileChooser()
+                    }
+
                 }
             }
         }
@@ -39,6 +63,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun FileChooser(){
+    var showFilePicker by remember { mutableStateOf(false) }
+
+    val fileType = "jpg"
+    FilePicker(showFilePicker) { path ->
+        showFilePicker = false
+        // do something with path
+    }
+
+    Button(
+        onClick = {
+            showFilePicker = true
+        },
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .height(48.dp)
+            .clickable { showFilePicker = true },
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        Text(text = "Choose File", style = TextStyle(color = Color.White))
+    }
 }
 
 @Preview(showBackground = true)

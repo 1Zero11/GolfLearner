4638fe016d0a5694e0de73b8f33f947b4cb274f9
Yurii Svetliakov <mroallll54@gmail.com>
2023-08-26 17:26:55 +0300

    Добавил пару моделей

--- app/src/main/java/com/vpered/golflearnner/MainActivity.kt
+++ app/src/main/java/com/vpered/golflearnner/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.vpered.golflearnner
 
-import android.content.Intent
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.BitmapFactory.Options
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,9 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,13 +29,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 import com.vpered.golflearnner.ui.theme.GolfLearnerTheme
+import org.jetbrains.kotlinx.dl.api.preprocessing.pipeline
+import org.jetbrains.kotlinx.dl.impl.preprocessing.TensorLayout
+import org.jetbrains.kotlinx.dl.impl.preprocessing.resize
+import org.jetbrains.kotlinx.dl.impl.preprocessing.toFloatArray
+import org.jetbrains.kotlinx.dl.onnx.inference.OnnxInferenceModel
+import org.jetbrains.kotlinx.dl.onnx.inference.executionproviders.ExecutionProvider
+import org.jetbrains.kotlinx.dl.onnx.inference.inferAndCloseUsing
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val modelBytes = resources.openRawResource(com.vpered.golflearnner.R.raw.kps_detector).readBytes()
+        Predict(modelBytes, resources)
+
         setContent {
             GolfLearnerTheme {
                 // A surface container using the 'background' color from the theme
@@ -45,22 +55,20 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column() {
-                        Greeting("Android")
+                        //Greeting("Android")
                         FileChooser()
                     }
 
                 }
             }
         }
-
-
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name!",
         modifier = modifier
     )
 }
@@ -68,11 +76,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun FileChooser(){
     var showFilePicker by remember { mutableStateOf(false) }
+    var pickedFileName by remember { mutableStateOf("") }
 
-    val fileType = "jpg"
-    FilePicker(showFilePicker) { path ->
+    val fileType = "mp4"
+    FilePicker(showFilePicker, fileExtensions = listOf(fileType)) { path ->
         showFilePicker = false
         // do something with path
+
+        if(path?.path != null)
+            pickedFileName = path.path;
+
     }
 
     Button(
@@ -88,6 +101,58 @@ fun FileChooser(){
     ) {
         Text(text = "Choose File", style = TextStyle(color = Color.White))
     }
+
+    if(pickedFileName != ""){
+        Text(text = "File Chosen: " + pickedFileName)
+    }
+
+}
+
+fun Predict(modelBytes: ByteArray, resources: Resources) {
+
+    val model = OnnxInferenceModel(modelBytes)
+
+    val preprocessing = pipeline<Bitmap>()
+        .resize {
+            outputHeight = 192
+            outputWidth = 192
+        }
+        .toFloatArray { layout = TensorLayout.NHWC }
+
+    val o = Options()
+    o.inScaled = false
+
+    val bitmap = BitmapFactory.decodeResource(resources, com.vpered.golflearnner.R.raw.golf, o)
+    val bitmap2 = BitmapFactory.decodeResource(resources, com.vpered.golflearnner.R.raw.golf_guy)
+
+    val detections = model.inferAndCloseUsing(ExecutionProvider.CPU()) {
+        var (inputData, shape) = preprocessing.apply(bitmap)
+
+        val (inputData2, _) = preprocessing.apply(bitmap2)
+
+        //inputData += inputData2
+        val test = arrayOf(inputData)
+        Log.d("MYTAG", "firstMessage")
+        val res = it.predictSoftly(inputData)
+
+
+
+        val resArr = Array(17) { i ->
+            FloatArray(3) { j ->
+                if(j==0) {
+                    res[i * 3 + j] * bitmap.width
+                } else if(j==1){
+                    res[i * 3 + j]*bitmap.height
+                } else{
+                    res[i * 3 + j]
+                }
+
+            }
+        }
+        Log.d("MYTAG", "Okay")
+    }
+
+
 }
 
 @Preview(showBackground = true)
